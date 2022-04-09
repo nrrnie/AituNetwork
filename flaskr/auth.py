0e0d342938d2d711291c6a70e8678ce0420c895f
nrrnie <72966907+nrrnie@users.noreply.github.com>
2022-04-09 10:48:42 +0600

    Add files via upload
    fixed

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,60 +1,65 @@
-import functools
-# flask
-from flask import
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-# security
-from werkzeug.security import check_password_hash, generate_password_hash
-# db.py
-from flaskr.db import get_db
+import mysql.connector, functools
+from flask import Blueprint, flash, g, redirect, render_template,
+    request, session, url_for
+from werkzeug.security import
+    check_password_hash, generate_password_hash
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    if request.method == 'POST'
-        email_address = request.form['email address']
-        username      = request.form['username']
-        password      = request.form['password']
-        db = get_db()
+    if request.method == 'POST':
+        email    = request.form['e-mail address']
+        username = request.form['username']
+        password = request.form['password']
         error = None
 
-        if not username: # if username is not given
-            error = 'Username is required'
-        elif not password: # same thing
-            error = 'Password is required'
-        elif not email_address: # same thing
+        if not email:
             error = 'E-mail address is required'
-        elif not re.search('@astanait.edu.kz', email_address):
-            # search AITU e-mail domain
-            error = 'University e-mail address is required'
+        elif not re.search('@astanait.edu.kz', email):
+            error = 'University e-mail (astanait.edu.kz) is required'
+        elif not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        
+        db = mysql.connector.connect(user='', database='')
+        cursor = db.cursor()
+        # add user
+        cursor.execute("INSERT INTO ...", (email, userame, generate_password_hash,))
 
         if error is None:
-            # no db right now, so just write
             return redirect(url_for("auth.login"))
-
         flash(error)
     return render_template('auth/register.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods=('GET','POST'))
 def login():
     if request.method == 'POST':
-        username = None
-        email_address = None
-        email_or_username = request.form['e-mail/username']
-        password = request.form['password']
+        email_or_username   = request.form['e-mail or username']
+        password            = request.form['password']
+        error = None;
 
-        if not re.search('@astanait.edu.kz', email_or_username):
-            # means it should be a username
-            username = email_or_username
+        # emptiness check
+        if not email_or_username:
+            error = 'E-mail or username are required'
+        elif not password:
+            error = 'Password is required'
+
+        db = mysql.connector.connect(user='', database='')
+        cursor = db.cursor()
+        
+        # email or username check
+        if not re.search('@astanait.edu.kz', email_or_username)
+            # assume it is username
+            db_request = '' #SELECT USERNAME ...
         else:
-            email_address = email_or_username
-        # again, db should be here
-        # db = get_db()
-        error = None
-        if username is None: # try to find
-            user = db.execute('', (email_address,)).fetchone(()
-        else:
-            user db.execute('', (username,)).fetchone()
+            # assume it is email
+            db_request = '' #SELECT EMAIL ...
+
+        user = cursor.execute(db_request, (email_or_username,)).fetchone()
 
         if user is None:
             error = 'Incorrect e-mail address or username'
@@ -63,16 +68,8 @@ def login():
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session[''] = user['id']
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
 
-
-
-
-
-
-
-
-                                         
